e6615ff7396ec53c0eb0e46803e741f8fd1f017c
Markus Hediger <m.hed@gmx.ch>
2018-02-18 14:00:26 +0100

    Check for defined User

--- lib/acts_as_versioner/userstamp.rb
+++ lib/acts_as_versioner/userstamp.rb
@@ -7,13 +7,15 @@ module Userstamp
   end
   
   def set_stamps
-    stamper = 0 # System
-    stamper = User.current_user.id if User && User.current_user
-    if self.id.blank?
-      self[ActiveRecord::Acts::Versioner::configurator[:default_versioned_created_by]] = stamper if self.has_attribute? ActiveRecord::Acts::Versioner::configurator[:default_versioned_created_by]
-      self[ActiveRecord::Acts::Versioner::configurator[:default_versioned_updated_by]] = stamper if self.has_attribute? ActiveRecord::Acts::Versioner::configurator[:default_versioned_updated_by]
-    else
-      self[ActiveRecord::Acts::Versioner::configurator[:default_versioned_updated_by]] = stamper if self.has_attribute? ActiveRecord::Acts::Versioner::configurator[:default_versioned_updated_by]
+    if defined?(User)
+      stamper = 0 # System
+      stamper = User.current_user.id if User.current_user
+      if self.id.blank?
+        self[ActiveRecord::Acts::Versioner::configurator[:default_versioned_created_by]] = stamper if self.has_attribute? ActiveRecord::Acts::Versioner::configurator[:default_versioned_created_by]
+        self[ActiveRecord::Acts::Versioner::configurator[:default_versioned_updated_by]] = stamper if self.has_attribute? ActiveRecord::Acts::Versioner::configurator[:default_versioned_updated_by]
+      else
+        self[ActiveRecord::Acts::Versioner::configurator[:default_versioned_updated_by]] = stamper if self.has_attribute? ActiveRecord::Acts::Versioner::configurator[:default_versioned_updated_by]
+      end
     end
   end
   
