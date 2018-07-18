e2d03b55eb585fd4ab421f6cda57e32f72cf5500
Markus Hediger <m.hed@gmx.ch>
2018-07-18 09:43:55 +0200

    Typo in ActiveRecord::Acts::Versioner::configurator

--- lib/acts_as_versioner.rb
+++ lib/acts_as_versioner.rb
@@ -3,10 +3,10 @@ require File.dirname(__FILE__) + '/acts_as_versioner/userstamp.rb'
 module ActiveRecord::Acts::Versioner
   @@configurator = {
     :default_versioned_class_name => '_versions',
-    :default_versiond_created_at => 'created_at',
-    :default_versiond_updated_at => 'updated_at',
-    :default_versiond_created_by => 'created_by',
-    :default_versiond_updated_by => 'updated_by'
+    :default_versioned_created_at => 'created_at',
+    :default_versioned_updated_at => 'updated_at',
+    :default_versioned_created_by => 'created_by',
+    :default_versioned_updated_by => 'updated_by'
   }
   mattr_reader :configurator
 end
